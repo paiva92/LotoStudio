d3d9bca1bdf23510f0f3c6a1f042cf9f4e506616
Daraguz <paivaalfonso@gmail.com>
2024-09-19 10:28:47 -0300

    update - tittles in contact and about pages

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,4 +2,16 @@
 	<title>About</title>
 </svelte:head>
 
-<h1>Sobre mi</h1>
+<div class="title animate delay-1"><h1 class="dancing">Sobre mi</h1></div>
+
+<style>
+	.title {
+		margin-top: 5vh;
+		display: flex;
+		justify-content: center;
+		h1 {
+			margin: 2vh;
+			font-size: 7vh;
+		}
+	}
+</style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,4 +2,16 @@
 	<title>Contact</title>
 </svelte:head>
 
-<h1>Contacto</h1>
+<div class="title animate delay-1"><h1 class="dancing">Contacto</h1></div>
+
+<style>
+	.title {
+		margin-top: 5vh;
+		display: flex;
+		justify-content: center;
+		h1 {
+			margin: 2vh;
+			font-size: 7vh;
+		}
+	}
+</style>
